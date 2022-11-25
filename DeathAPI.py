517499ee858647743a60fe1f704d3e8b5b57fc41
Colten Buzard <colten.buz@gmail.com>
2022-11-25 15:28:04 -0500

    Copied code from documentation and created functions to call data

--- DeathAPI.py
+++ DeathAPI.py
@@ -1,1 +1,182 @@
-#comment1
+from CDChelperfunctions import *
+
+#comment2testing commit
+
+#B - Group By Parameters
+# b_parameters = group_By('year','injuryintent','icdchapter','icdsub-chapter','causeofdeath')
+
+#M - Values to measure by
+m_parameters = measures('deaths','pop','percentofdeaths')
+
+#F - Don't change these
+f_parameters = {
+    "F_D76.V1": ["*All*"], # year/month
+    "F_D76.V10": ["*All*"], # Census Regions - dont change
+    "F_D76.V2": ["*All*"], # ICD-10 Codes
+    "F_D76.V27": ["*All*"], # HHS Regions - dont change
+    "F_D76.V9": ["*All*"] # State County - dont change
+}
+
+#I - parameters
+i_parameters = {
+    "I_D76.V1": "2022 2022",  # year/month
+    "I_D76.V10": "*All* (The United States)", # Census Regions - dont change
+    "I_D76.V2": "*All*", # ICD-10 Codes
+    "I_D76.V27": "*All* (The United States)", # HHS Regions - dont change
+    "I_D76.V9": "*All* (The United States)" # State County - dont change
+}
+
+# variable values to limit in the "where" clause of the query, found in multiple select 
+# list boxes and advanced finder text entry boxes in the "Request Form."
+
+# For this example, we want to include ten-year age groups for ages 15-44.
+# For all other categories, include all values
+
+
+v_parameters = {
+    "V_D76.V1": "",         # Year/Month
+    "V_D76.V10": "",        # Census Regions
+    "V_D76.V11": "*All*",   # 2006 Urbanization
+    "V_D76.V12": "*All*",   # ICD-10 130 Cause List (Infants)
+    "V_D76.V17": "*All*",   # Hispanic Origin
+    "V_D76.V19": "*All*",   # 2013 Urbanization
+    "V_D76.V2": "",         # ICD-10 Codes
+    "V_D76.V20": "*All*",   # Autopsy
+    "V_D76.V21": "*All*",   # Place of Death
+    "V_D76.V22": "*All*",   # Injury Intent
+    "V_D76.V23": "*All*",   # Injury Mechanism and All Other Leading Causes
+    "V_D76.V24": "*All*",   # Weekday
+    "V_D76.V25": "*All*",   # Drug/Alcohol Induced Causes
+    "V_D76.V27": "",        # HHS Regions
+    "V_D76.V4": "*All*",    # ICD-10 113 Cause List
+    "V_D76.V5": ["*All*"], # Ten-Year Age Groups
+    "V_D76.V51": "*All*",   # Five-Year Age Groups
+    "V_D76.V52": "*All*",   # Single-Year Ages
+    "V_D76.V6": "00",       # Infant Age Groups
+    "V_D76.V7": "*All*",    # Gender
+    "V_D76.V8": "*All*",    # Race
+    "V_D76.V9": ""          # State/County
+}
+
+# other parameters, such as radio buttons, checkboxes, and lists that are not data categories
+
+# For this example, include age-adjusted rates, use ten-year age groups (D76.V5), use state location by default, 
+# show rates per 100,000, use 2013 urbanization and use ICD-10 Codes (D76.V2) for cause of death category
+title = '2022 CDC Death Data'
+
+o_parameters = {
+    "O_V10_fmode": "freg",    # Use regular finder and ignore v parameter value
+    "O_V1_fmode": "freg",     # Use regular finder and ignore v parameter value
+    "O_V27_fmode": "freg",    # Use regular finder and ignore v parameter value
+    "O_V2_fmode": "freg",     # Use regular finder and ignore v parameter value
+    "O_V9_fmode": "freg",     # Use regular finder and ignore v parameter value
+    "O_aar": "aar_std",       # age-adjusted rates
+    "O_aar_pop": "0000",      # population selection for age-adjusted rates
+    "O_age": "D76.V5",        # select age-group (e.g. ten-year, five-year, single-year, infant groups)
+    "O_javascript": "on",     # Set to on by default
+    "O_location": "D76.V9",   # select location variable to use (e.g. state/county, census, hhs regions)
+    "O_precision": "1",       # decimal places
+    "O_rate_per": "100000",   # rates calculated per X persons
+    "O_show_totals": "false",  # Show totals for 
+    "O_timeout": "300",
+    "O_title": title,    # title for data run
+    "O_ucd": "D76.V2",        # select underlying cause of death category
+    "O_urban": "D76.V19"      # select urbanization category
+}
+
+# values for non-standard age adjusted rates (see mortality online databases).
+# For this example, these parameters are ignored as standard age adjusted rates are used
+vm_parameters = {
+    "VM_D76.M6_D76.V10": "",        # Location
+    "VM_D76.M6_D76.V17": "*All*",   # Hispanic-Origin
+    "VM_D76.M6_D76.V1_S": "*All*",  # Year
+    "VM_D76.M6_D76.V7": "*All*",    # Gender
+    "VM_D76.M6_D76.V8": "*All*"     # Race
+}
+
+
+# Miscellaneous hidden inputs/parameters usually passed by web form. These do not change.
+misc_parameters = {
+    "action-Send": "Send",
+    "finder-stage-D76.V1": "codeset",
+    "finder-stage-D76.V1": "codeset",
+    "finder-stage-D76.V2": "codeset",
+    "finder-stage-D76.V27": "codeset",
+    "finder-stage-D76.V9": "codeset",
+    "stage": "request"
+}
+
+xml_request = "\n"
+xml_request += createParameterList(b_parameters)
+xml_request += createParameterList(m_parameters)
+xml_request += createParameterList(f_parameters)
+xml_request += createParameterList(i_parameters)
+xml_request += createParameterList(o_parameters)
+xml_request += createParameterList(vm_parameters)
+xml_request += createParameterList(v_parameters)
+xml_request += createParameterList(misc_parameters)
+xml_request += ""
+
+import requests
+
+url = "https://wonder.cdc.gov/controller/datarequest/D176"
+response = requests.post(url, data={"request_xml": xml_request, "accept_datause_restrictions": "true"})
+
+if response.status_code == 200:
+    data = response.text
+else:
+    print("something went wrong")
+
+# BeautifulSoup library facilitates parsing of XML response
+import bs4 as bs
+
+# This library faciliates 2-dimensional array operations and visualization
+import pandas as pd
+
+def xml2df(xml_data):
+    """ This function grabs the root of the XML document and iterates over
+        the 'r' (row) and 'c' (column) tags of the data-table
+        Rows with a 'v' attribute contain a numerical value
+        Rows with a 'l attribute contain a text label and may contain an
+        additional 'r' (rowspan) tag which identifies how many rows the value
+        should be added. If present, that label will be added to the following
+        rows of the data table.
+    
+        Function returns a two-dimensional array or data frame that may be 
+        used by the pandas library."""
+    
+    root = bs.BeautifulSoup(xml_data,"lxml")
+    all_records = []
+    row_number = 0
+    rows = root.find_all("r")
+    
+    for row in rows:
+        if row_number >= len(all_records):
+            all_records.append([])
+              
+        for cell in row.find_all("c"):
+            if 'v' in cell.attrs:
+                try:
+                    all_records[row_number].append(float(cell.attrs["v"].replace(',','')))
+                except ValueError:
+                    all_records[row_number].append(cell.attrs["v"])
+            else:
+                if 'r' not in cell.attrs:
+                    all_records[row_number].append(cell.attrs["l"])
+                else:
+                
+                    for row_index in range(int(cell.attrs["r"])):
+                        if (row_number + row_index) >= len(all_records):
+                            all_records.append([])
+                            all_records[row_number + row_index].append(cell.attrs["l"])
+                        else:
+                            all_records[row_number + row_index].append(cell.attrs["l"])
+                                           
+        row_number += 1
+    return all_records
+
+# data_frame = xml2df(data)
+
+# df = pd.DataFrame(data=data_frame, columns=["Year", "Race", "Deaths", "Population", "Crude Rate", "Age-adjusted Rate", "Age-adjusted Rate Standard Error"])
+
+# df.head()
